3c999aeda967039a1f3e54007902293e39efbadd
zlq <1542498005@qq.com>
2024-11-07 20:52:36 +0800

    Update the game

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Graphics/Text.hpp>
+#include <SFML/System/Vector2.hpp>
+// #include <math.h>
+#include <cstddef>
 #include <deque>
 #include <queue>
 #include <random>
@@ -21,6 +24,7 @@ public:
     void setselectedItemIndex();
     int getPressedItem() const { return selectedItemIndex; }
     void reset();
+    void reset1();
     void draw_a(float width, float height);
 
 private:
@@ -49,6 +53,17 @@ void Menu::reset() {
     menuItems.emplace_back("Green", font, 50);
     menuItems.emplace_back("Blue", font, 50);
     menuItems.emplace_back("Yellow", font, 50);
+
+    selectedItemIndex = 0;
+}
+
+void Menu::reset1() {
+    while (!menuItems.empty()) {
+        menuItems.pop_back();
+    }
+
+    menuItems.emplace_back("One", font, 50);
+    menuItems.emplace_back("Two", font, 50);
 
     selectedItemIndex = 0;
 }
@@ -88,73 +103,122 @@ void Menu::moveDown() {
     }
 }
 
-enum Direction { Up, Down, Left, Right };
+enum Direction { Up, Down, Left, Right, W, S, A, D };
 
 struct Snake {
-    std::deque<sf::Vector2i> body;
-    Direction dir;
+    std::deque<sf::Vector2i> body[2];
+
+    Direction dir[2];
+    std::queue<Direction> Myqueue[2];
 };
 
-void resetSnake(Snake &snake) {
+void resetSnake(Snake &snake, int snake_index) {
 
-    snake.body.clear();
-    snake.body.emplace_back(10, 10);
-    snake.dir = Right;
+    snake.body[snake_index].clear();
+    snake.body[snake_index].emplace_back(10, 10 + 10 * snake_index);
+    snake.dir[snake_index] = Right;
 }
+// void resetSnake1(Snake &snake) {
+//     snake.body[1].clear();
+//     snake.body[1].emplace_back(10, 20);
+//     snake.dir[1] = Right;
+// }
+// void drawscore(size_t length, int num) {
+//     std::vector<sf::Text> scoreItems;
+//     sf::Font font;
+//     scoreItems.emplace_back(
+//         "Player " + std::to_string(num) + ":" + std::to_string(length), font, 50);
+//     scoreItems[0].setPosition(float(20) + float((num - 1) * 400), 700);
+//     scoreItems[0].setFillColor(sf::Color::White);
 
-void moveSnake(Snake &snake) {
-    sf::Vector2i newHead = snake.body.front();
+//     while (!scoreItems.empty()) {
+//         scoreItems.pop_back();
 
-    switch (snake.dir) {
-    case Up:
-        newHead.y--;
-        break;
-    case Down:
-        newHead.y++;
-        break;
-    case Left:
-        newHead.x--;
-        break;
-    case Right:
-        newHead.x++;
-        break;
+//     }
+// }
+void moveSnake(Snake &snake, int num) {
+
+    sf::Vector2i newHead[2];
+
+    for (int i = 0; i <= num - 1; i++) {
+        newHead[i] = snake.body[i].front();
+        Direction clone_dir;
+        clone_dir = snake.dir[i];
+        if (snake.dir[i] == W) {
+            clone_dir = Up;
+        }
+        if (snake.dir[i] == A) {
+            clone_dir = Left;
+        }
+        if (snake.dir[i] == D) {
+            clone_dir = Right;
+        }
+        if (snake.dir[i] == S) {
+            clone_dir = Down;
+        }
+
+        switch (clone_dir) {
+        case Up:
+            newHead[i].y--;
+            break;
+        case Down:
+            newHead[i].y++;
+            break;
+        case Left:
+            newHead[i].x--;
+            break;
+        case Right:
+            newHead[i].x++;
+            break;
+        default:
+            break;
+        }
+
+        snake.body[i].push_front(newHead[i]);
+        snake.body[i].pop_back();
     }
-
-    snake.body.push_front(newHead);
-    snake.body.pop_back();
 }
 
-bool isCollision(const Snake &snake) {
-    const sf::Vector2i &head = snake.body.front();
+bool isCollision(const Snake &snake, int num, int snake_index) {
+
+    const sf::Vector2i head = snake.body[snake_index].front();
 
     if (head.x < 0 || head.x >= WIDTH / CELL_SIZE || head.y < 0 || head.y >= HEIGHT / CELL_SIZE)
         return true;
-
-    for (size_t i = 1; i < snake.body.size(); ++i) {
-        if (snake.body[i] == head)
-            return true;
+    for (int j = 0; j <= num - 1; j++) {
+        if (j == snake_index) {
+            for (size_t i = 1; i < snake.body[j].size(); ++i) {
+                if (snake.body[j][i] == head)
+                    return true;
+            }
+        }
+        if (j != snake_index) {
+            for (const auto &body : snake.body[j]) {
+                if (body == head)
+                    return true;
+            }
+        }
     }
 
     return false;
 }
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Snake Game");
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT + 200), "Snake Game");
     Snake snake;
-    std::queue<Direction> Myqueue;
+    std::queue<Direction> Myqueue[2];
+    int num = 1;
     int ge = 0;
-    sf::Color snakeColor = sf::Color::Green;
+    sf::Color snakeColor[2] = {sf::Color::Green, sf::Color::Yellow};
     std::random_device rd;  // 获取随机数种子
     std::mt19937 gen(rd()); // 初始化梅森旋转算法的生成器
 
-    // 定义范围
     std::uniform_int_distribution<int> distX(0, WIDTH / CELL_SIZE - 1);
     std::uniform_int_distribution<int> distY(0, HEIGHT / CELL_SIZE - 1);
 
     Menu menu;
     bool inMenu = true;
 
-    resetSnake(snake);
     sf::Clock clock;
     sf::Vector2i food(distX(gen), distY(gen));
 
@@ -174,7 +238,10 @@ int main() {
                     } else if (event.key.code == sf::Keyboard::Enter) {
                         switch (menu.getPressedItem() + ge) {
                         case 0:
-                            inMenu = false;
+                            window.clear();
+                            ge += 6;
+                            menu.reset1();
+                            // inMenu = false;
                             break;
                         case 1:
                             window.clear();
@@ -188,11 +255,21 @@ int main() {
                             inMenu = false;
                             break;
                         case 4:
-                            snakeColor = sf::Color::Blue;
+                            snakeColor[0] = sf::Color::Blue;
                             inMenu = false;
                             break;
                         case 5:
-                            snakeColor = sf::Color::Yellow;
+                            snakeColor[0] = sf::Color::Yellow;
+                            inMenu = false;
+                            break;
+                        case 6:
+                            resetSnake(snake, 0);
+                            inMenu = false;
+                            break;
+                        case 7:
+                            num = 2;
+                            resetSnake(snake, 0);
+                            resetSnake(snake, 1);
                             inMenu = false;
                             break;
                         }
@@ -209,67 +286,130 @@ int main() {
             if (clock.getElapsedTime().asSeconds() > SPEED) {
 
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-                    if ((Myqueue.empty() && Myqueue.size() < 2 && snake.dir != Down &&
-                         snake.dir != Up) ||
-                        (Myqueue.front() != Down && Myqueue.size() == 1 && Myqueue.front() != Up)) {
-                        Myqueue.push(Up);
+                    if ((Myqueue[0].empty() && Myqueue[0].size() < 2 && snake.dir[0] != Down &&
+                         snake.dir[0] != Up) ||
+                        (Myqueue[0].front() != Down && Myqueue[0].size() == 1 &&
+                         Myqueue[0].front() != Up)) {
+                        Myqueue[0].push(Up);
                     }
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-                    if ((Myqueue.empty() && Myqueue.size() < 2 && snake.dir != Down &&
-                         snake.dir != Up) ||
-                        (Myqueue.front() != Down && Myqueue.size() == 1 && Myqueue.front() != Up)) {
-                        Myqueue.push(Down);
+                    if ((Myqueue[0].empty() && Myqueue[0].size() < 2 && snake.dir[0] != Down &&
+                         snake.dir[0] != Up) ||
+                        (Myqueue[0].front() != Down && Myqueue[0].size() == 1 &&
+                         Myqueue[0].front() != Up)) {
+                        Myqueue[0].push(Down);
                     }
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-                    if ((Myqueue.empty() && Myqueue.size() < 2 && snake.dir != Left &&
-                         snake.dir != Right) ||
-                        (Myqueue.front() != Left && Myqueue.size() == 1 &&
-                         Myqueue.front() != Right)) {
-                        Myqueue.push(Left);
+                    if ((Myqueue[0].empty() && Myqueue[0].size() < 2 && snake.dir[0] != Left &&
+                         snake.dir[0] != Right) ||
+                        (Myqueue[0].front() != Left && Myqueue[0].size() == 1 &&
+                         Myqueue[0].front() != Right)) {
+                        Myqueue[0].push(Left);
                     }
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-                    if ((Myqueue.empty() && Myqueue.size() < 2 && snake.dir != Left &&
-                         snake.dir != Right) ||
-                        (Myqueue.front() != Left && Myqueue.size() == 1 &&
-                         Myqueue.front() != Right)) {
-                        Myqueue.push(Right);
+                    if ((Myqueue[0].empty() && Myqueue[0].size() < 2 && snake.dir[0] != Left &&
+                         snake.dir[0] != Right) ||
+                        (Myqueue[0].front() != Left && Myqueue[0].size() == 1 &&
+                         Myqueue[0].front() != Right)) {
+                        Myqueue[0].push(Right);
                     }
                 }
-
-                if (!Myqueue.empty()) {
-                    snake.dir = Myqueue.front();
-                    Myqueue.pop();
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+                    if ((Myqueue[1].empty() && Myqueue[1].size() < 2 && snake.dir[1] != S &&
+                         snake.dir[1] != W) ||
+                        (Myqueue[1].front() != S && Myqueue[1].size() == 1 &&
+                         Myqueue[1].front() != W)) {
+                        Myqueue[1].push(W);
+                    }
                 }
-
-                moveSnake(snake);
-
-                if (isCollision(snake)) {
-                    resetSnake(snake);
-                } else if (snake.body.front() == food) {
-                    snake.body.push_back(snake.body.back());
-                    std::uniform_int_distribution<int> distX(0, WIDTH / CELL_SIZE - 1);
-                    std::uniform_int_distribution<int> distY(0, HEIGHT / CELL_SIZE - 1);
-
-                    food = sf::Vector2i(distX(gen), distY(gen));
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+                    if ((Myqueue[1].empty() && Myqueue[1].size() < 2 && snake.dir[1] != S &&
+                         snake.dir[1] != W) ||
+                        (Myqueue[1].front() != S && Myqueue[1].size() == 1 &&
+                         Myqueue[1].front() != W)) {
+                        Myqueue[1].push(S);
+                    }
                 }
-                clock.restart();
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+                    if ((Myqueue[1].empty() && Myqueue[1].size() < 2 && snake.dir[1] != A &&
+                         snake.dir[1] != D) ||
+                        (Myqueue[1].front() != A && Myqueue[1].size() == 1 &&
+                         Myqueue[1].front() != D)) {
+                        Myqueue[1].push(A);
+                    }
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+                    if ((Myqueue[1].empty() && Myqueue[1].size() < 2 && snake.dir[1] != A &&
+                         snake.dir[1] != D) ||
+                        (Myqueue[1].front() != A && Myqueue[1].size() == 1 &&
+                         Myqueue[1].front() != D)) {
+                        Myqueue[1].push(D);
+                    }
+                }
+                for (int i = 0; i <= num - 1; i++) {
+
+                    if (!Myqueue[i].empty()) {
+                        snake.dir[i] = Myqueue[i].front();
+                        Myqueue[i].pop();
+                    }
+                }
+                moveSnake(snake, num);
+                if (num == 2) {
+                    if (isCollision(snake, 2, 0) && isCollision(snake, 2, 1)) {
+                        resetSnake(snake, 0);
+                        resetSnake(snake, 1);
+                    }
+                }
+                for (int i = 0; i <= num - 1; i++) {
+                    if (isCollision(snake, num, i)) {
+                        resetSnake(snake, i);
+                    } else if (snake.body[i].front() == food) {
+                        snake.body[i].push_back(snake.body[i].back());
+                        std::uniform_int_distribution<int> distX(0, WIDTH / CELL_SIZE - 1);
+                        std::uniform_int_distribution<int> distY(0, HEIGHT / CELL_SIZE - 1);
+
+                        food = sf::Vector2i(distX(gen), distY(gen));
+                    }
+                    clock.restart();
+                }
             }
 
             window.clear();
             sf::RectangleShape cell(sf::Vector2f(CELL_SIZE - 1, CELL_SIZE - 1));
+            sf::RectangleShape wall(sf::Vector2f(CELL_SIZE * 40, CELL_SIZE));
+            wall.setPosition(0, 600);
+            wall.setFillColor(sf::Color::White);
+            window.draw(wall);
+            for (int i = 0; i <= num - 1; i++) {
+                for (const auto &segment : snake.body[i]) {
+                    cell.setPosition(float(segment.x) * CELL_SIZE, float(segment.y) * CELL_SIZE);
+                    cell.setFillColor(snakeColor[i]);
 
-            for (const auto &segment : snake.body) {
-                cell.setPosition(float(segment.x) * CELL_SIZE, float(segment.y) * CELL_SIZE);
-                cell.setFillColor(snakeColor);
-                window.draw(cell);
+                    window.draw(cell);
+                }
             }
-
             cell.setPosition(float(food.x) * CELL_SIZE, float(food.y) * CELL_SIZE);
             cell.setFillColor(sf::Color::Red);
             window.draw(cell);
+            for (int i = 0; i <= num - 1; i++) {
+                size_t length = snake.body[i].size();
+                std::vector<sf::Text> scoreItems;
+                sf::Font font;
+                if (!font.loadFromFile("/usr/share/fonts/truetype/msttcorefonts/Arial.ttf")) {
+                }
+                scoreItems.emplace_back(
+                    "Player " + std::to_string(i + 1) + "'s score:" + std::to_string(length), font,
+                    40);
+                scoreItems[0].setPosition(float(20) + float((i)*400), 700);
+                scoreItems[0].setFillColor(sf::Color::White);
+                window.draw(scoreItems[0]);
+                while (!scoreItems.empty()) {
+                    scoreItems.pop_back();
+                }
+            }
         }
 
         window.display();
